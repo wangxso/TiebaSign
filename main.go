b367890eb8599b696acff9f1830132f27e4fc27a
wangxs <wangxinsheng0102@163.com>
2022-07-07 22:03:17 +0800

    add log to file

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"crypto/md5"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"math/rand"
+	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -31,7 +33,7 @@ var session *nic.Session
 var wg sync.WaitGroup
 
 func init() {
-	logrus.SetLevel(logrus.TraceLevel)
+	InitLog()
 	session = &nic.Session{}
 
 	yamlFile, err := ioutil.ReadFile("./config.yaml")
@@ -43,6 +45,26 @@ func init() {
 		logrus.Error("unmarshal: " + err.Error())
 	}
 	logrus.Info("load bduss: ", config.BDUSS)
+}
+
+func InitLog() {
+	//设置输出样式，自带的只有两种样式logrus.JSONFormatter{}和logrus.TextFormatter{}
+	logrus.SetFormatter(&logrus.TextFormatter{})
+	logrus.SetOutput(os.Stdout)
+	//设置output,默认为stderr,可以为任何io.Writer，比如文件*os.File
+	file, err := os.OpenFile("tieba_sign.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	writers := []io.Writer{
+		file,
+		os.Stdout}
+	//同时写文件和屏幕
+	fileAndStdoutWriter := io.MultiWriter(writers...)
+	if err == nil {
+		logrus.SetOutput(fileAndStdoutWriter)
+	} else {
+		logrus.Info("failed to log to file.")
+	}
+	//设置最低loglevel
+	logrus.SetLevel(logrus.InfoLevel)
 }
 
 func getTbs(bduss string) (tbs string) {
